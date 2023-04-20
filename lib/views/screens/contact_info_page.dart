07d7053f89a1aad1dcc3af5901eb66769b16d621
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2023-04-20 10:55:44 +0530

    JG | 20/04/2023 13.5

--- lib/views/screens/contact_info_page.dart
+++ lib/views/screens/contact_info_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:resume_builder_jcf10/utils/icon_utils.dart';
 import 'package:resume_builder_jcf10/utils/theme_utils.dart';
 import 'package:resume_builder_jcf10/views/component/back_button.dart';
 
@@ -14,12 +16,20 @@ class _ContactInfoPageState extends State<ContactInfoPage> {
 
   int index = 0;
 
+  bool hide = true;
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    double iconSize = size.height * 0.04;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "ContactInfoPage",
+          "ContactInfo Page",
           style: appBarTitleStyle,
         ),
         leading: const MyBackButton(),
@@ -28,6 +38,7 @@ class _ContactInfoPageState extends State<ContactInfoPage> {
       ),
       body: Column(
         children: [
+          //TabBar
           Expanded(
             child: Row(
               children: [
@@ -91,7 +102,149 @@ class _ContactInfoPageState extends State<ContactInfoPage> {
               child: IndexedStack(
                 index: index,
                 children: [
-                  Text("Contact Page"),
+                  //Contact Page
+                  Container(
+                    padding: const EdgeInsets.all(18),
+                    color: Colors.white,
+                    child: Form(
+                      key: formKey,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Image.asset(
+                                    iconPath + "user.png",
+                                    height: iconSize,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  flex: 8,
+                                  child: TextFormField(
+                                    textInputAction: TextInputAction.next,
+                                    decoration: InputDecoration(
+                                      hintText: "Enter name",
+                                      labelText: "Name",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  iconPath + "email.png",
+                                  height: iconSize,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: TextFormField(
+                                    textInputAction: TextInputAction.next,
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: InputDecoration(
+                                      hintText: "Enter email",
+                                      labelText: "Email",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  iconPath + "smartphone-call.png",
+                                  height: iconSize,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: TextFormField(
+                                    maxLength: 10,
+                                    textInputAction: TextInputAction.next,
+                                    keyboardType: TextInputType.phone,
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.digitsOnly,
+                                    ],
+                                    decoration: const InputDecoration(
+                                      hintText: "Enter mobile number",
+                                      labelText: "Mobile no.",
+                                      prefixText: "+91 ",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  iconPath + "pin.png",
+                                  height: iconSize,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: TextFormField(
+                                    textInputAction: TextInputAction.next,
+                                    decoration: InputDecoration(),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Spacer(),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  flex: 8,
+                                  child: TextFormField(),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Spacer(),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  flex: 8,
+                                  child: TextFormField(),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text("Save"),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text("Reset"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  //Photo Page
                   Text("Image Page"),
                 ],
               ),
@@ -99,6 +252,7 @@ class _ContactInfoPageState extends State<ContactInfoPage> {
           ),
         ],
       ),
+      backgroundColor: Colors.grey.shade200,
     );
   }
 }
